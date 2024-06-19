99bc9d5c88ddc87011d3354b42e1575eee4355ac
Mawuli Agbeko <jmmyagbeko@st.ug.edu.gh>
2024-06-18 23:42:46 -1200

    Project completed

--- Jobiz/Component/homepage.js
+++ Jobiz/Component/homepage.js
@@ -188,6 +188,63 @@ export default function HomePage() {
               </ImageBackground>
             </ScrollView>
           </View>
+          <View style={styles.jobsAll}>
+            <Text style={styles.featured}>Popular Jobs</Text>
+            <TouchableOpacity>
+              <Text style={styles.see}>See all</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.verticallistContainer}>
+            
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/burgerking.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Jr Executive</Text><Text style={styles.burgerAngels}>Burger King</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.Byear}>$96,000/y</Text><Text style={styles.burgerAngels}>Los Angels, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/beats.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Product Manager</Text><Text style={styles.burgerAngels}>Beats</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.year}>$84,000/y</Text><Text style={styles.burgerAngels}>Florida, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/facebook.jpeg")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Product Manager</Text><Text style={styles.burgerAngels}>Facebook</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.year}>$86,000/y</Text><Text style={styles.burgerAngels}>Florida, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/linkedin.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Jr Executive</Text><Text style={styles.burgerAngels}>Linkedin</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.Byear}>$80,000/y</Text><Text style={styles.burgerAngels}>Berlin, Germany</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/adobe.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Product Manager</Text><Text style={styles.burgerAngels}>Adobe</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.year}>$96,000/y</Text><Text style={styles.burgerAngels}>Florida, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/uber.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>UI/UX Designer</Text><Text style={styles.burgerAngels}>Uber</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.Byear}>$96,000/y</Text><Text style={styles.burgerAngels}>Los Angels, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/paypal.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Business Analyst</Text><Text style={styles.burgerAngels}>Burger King</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.Byear}>$96,000/y</Text><Text style={styles.burgerAngels}>Los Angels, US</Text></View>
+            </View>
+
+            <View style={styles.popularjobs}>
+              <Image style={styles.BBF} source={require("../assets/pinterest.png")}/>
+              <View style={styles.executvieContainer}><Text style={styles.jr}>Resource Person</Text><Text style={styles.burgerAngels}>Burger King</Text></View>
+              <View style={styles.stateContainer}><Text style={styles.Byear}>$50,000/y</Text><Text style={styles.burgerAngels}>Carlifonia, US</Text></View>
+            </View>
+
+          </View>
         
         
         
@@ -201,12 +258,12 @@ export default function HomePage() {
 const styles = StyleSheet.create({
   hpcontainer: {
     flex: 1,
-    padding: 18,
+    padding: 30,
   },
   EA: {
-    marginTop: 40,
+    marginTop: 15,
     flexDirection: 'row',
-    gap: 115,
+    gap: 100,
   },
   ericgmail: {
     flexDirection: 'column',
@@ -274,8 +331,8 @@ const styles = StyleSheet.create({
   },
   jobsAll: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     marginTop: 50,
+    gap: 200
   },
   featured: {
     fontSize: 18,
@@ -338,4 +395,44 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '600'
   },
+  verticallistContainer: {
+    flexDirection: 'column',
+    marginTop: 30,
+    gap: 15,
+  },
+  popularjobs: {
+    flexDirection: 'row',
+    borderRadius: 20,
+    gap: 10,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  BBF: {
+    height: 50,
+    width: 50,
+    marginTop: 5,
+    borderRadius: 40,
+  },
+  executvieContainer: {
+    gap: 15,
+    marginTop: 10,
+  },
+  stateContainer: {
+    marginLeft: 70,
+    marginTop: 10,
+    gap: 15
+  },
+  jr: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  burgerAngels: {
+    fontSize: 14,
+    color: '#95969D'
+  },
+  Byear: { 
+    marginLeft: 25,
+
+  },
+  
 });
